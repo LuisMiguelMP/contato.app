be97a598adf574aac5f367c846c30c2da4673f5a
Luís Miguel Massih Pereira <lmmp1337@outlook.com>
2020-11-17 13:10:12 -0300

    fix manifests to multiple PWA on same domain

---
+++ web/sw.js
@@ -0,0 +1,22 @@
+const cacheVersion = 'v1';
+const urlsToPrefetch = [
+  'https://contato.app',
+  'https://contato.app/miguel',
+];
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheVersion).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
+    })
+  );
+});
+
+
+this.addEventListener('fetch', event => {
+  let responsePromise = caches.match(event.request).then(response => {
+    return response || fetch(event.request)
+  });
+
+  event.respondWith(responsePromise);
+});
